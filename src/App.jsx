cb37d6e22ef45f24cd31a9f7a5c4f073c9ed3d4f
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-12-16 13:37:47 +0530

    setting up chakraUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Button>
+        Hello
+      </Button>
+    </div>
+  )
 }
 
 export default App;
