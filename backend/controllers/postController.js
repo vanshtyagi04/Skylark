62404d62a22ee49f824bde212d9ca4d54efb520b
Ayush-Gupta-bit <ag6700717@gmail.com>
2024-12-19 13:09:20 +0530

    Designed conversation and message models and some controllers and routes.

---
+++ backend/controllers/postController.js
@@ -0,0 +1,145 @@
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
+import { v2 as cloudinary } from "cloudinary";
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+const createPost = asyncHandler(async (req, res) => {
+    const { postedBy, text } = req.body;
+    let { img } = req.body;
+
+    if (!postedBy || !text) {
+        return res.status(400).json({ error: "Postedby and text fields are required" });
+    }
+
+    const user = await User.findById(postedBy);
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+
+    if (user._id.toString() !== req.user._id.toString()) {
+        return res.status(401).json({ error: "Unauthorized to create post" });
+    }
+
+    const maxLength = 500;
+    if (text.length > maxLength) {
+        return res.status(400).json({ error: `Text must be less than ${maxLength} characters` });
+    }
+
+    if (img) {
+        const uploadedResponse = await cloudinary.uploader.upload(img);
+        img = uploadedResponse.secure_url;
+    }
+
+    const newPost = new Post({ postedBy, text, img });
+    await newPost.save();
+
+    res.status(201).json(newPost);
+});
+
+const getPost = asyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+
+    res.status(200).json(post);
+});
+
+const deletePost = asyncHandler(async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+        return res.status(401).json({ error: "Unauthorized to delete post" });
+    }
+
+    if (post.img) {
+        const imgId = post.img.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+});
+
+
+const likeUnlikePost = asyncHandler(async (req, res) => {
+    const { id: postId } = req.params;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+        await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+        res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+        post.likes.push(userId);
+        await post.save();
+        res.status(200).json({ message: "Post liked successfully" });
+    }
+});
+
+const replyToPost = asyncHandler(async (req, res) => {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const userProfilePic = req.user.profilePic;
+    const username = req.user.username;
+
+    if (!text) {
+        return res.status(400).json({ error: "Text field is required" });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+    }
+
+    const reply = { userId, text, userProfilePic, username };
+
+    post.replies.push(reply);
+    await post.save();
+
+    res.status(200).json(reply);
+});
+
+const getFeedPosts = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
+
+    res.status(200).json(feedPosts);
+});
+
+const getUserPosts = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+
+    const posts = await Post.find({ postedBy: user._id }).sort({ createdAt: -1 });
+
+    res.status(200).json(posts);
+});
+
+export { createPost, getPost, deletePost, likeUnlikePost, replyToPost, getFeedPosts, getUserPosts };
