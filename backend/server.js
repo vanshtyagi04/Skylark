a522bd8e51d8e2bb081e74e7f88a80f547586b33
vanshtyagi04 <vanshtyagi4526@gmail.com>
2024-12-26 15:42:34 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,7 @@ dotenv.config();
 connectDB();
 
 const PORT=process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -26,5 +28,12 @@ app.use(cookieParser());
 app.use("/api/users",userRoutes)
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req , res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 server.listen(PORT);
